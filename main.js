0c5bcdf997e415fc048a931c551a64e31cfa5f9e
Penguin <phuvis7065@gmail.com>
2020-02-21 14:19:25 +0700

    test

---
+++ main.js
@@ -0,0 +1,22 @@
+
+/*
+var sum =0 ;
+for(var i =0;i<100;i++){
+    if(i %2 == 0){
+        sum = sum+i;
+    }
+
+}
+console.log(sum);
+*/
+var i =0;
+var sum = 0;
+while(i<100){
+    i++;
+    if(i%2 ==0){
+        sum = sum+i
+    }
+    
+}
+
+console.log(sum);
